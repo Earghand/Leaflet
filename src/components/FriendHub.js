c44361f12f1e52686e4c56a398d88521169ad921
Wesley-T-Wang <Wesley.wang.t@gmail.com>
2021-04-10 16:48:30 -0700

    hub page

---
+++ src/components/FriendHub.js
@@ -0,0 +1,17 @@
+import React from 'react';
+ 
+const home = () => {
+    return (
+       <div>
+          <h1>Hub for friends</h1>
+          
+       </div>
+
+       
+    );
+}
+ 
+export default home;
+
+// import Hub from './components/FriendHub';
+// <Route path='/hub' component={Hub}/>
